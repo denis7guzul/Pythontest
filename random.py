a54e6196a02e65997afb9ddaef535b58b3f13e9f
Dinis <153553701+denis7guzul@users.noreply.github.com>
2023-12-26 10:55:45 -0500

    Create random.py

---
+++ random.py
@@ -0,0 +1,11 @@
+# Імпортуємо необхідні модулі
+import random
+
+# Визначаємо функцію для генерування випадкового числа
+def generate_random_number():
+  return random.randint(1, 100)
+
+# Викликаємо функцію
+print(generate_random_number())
+
+# Виводимо результат
